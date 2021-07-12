a0da3b02f7765d2ad0c5173c775709252a92a199
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-07-12 14:47:28 +0530

    virtual env commands

---
+++ expert/workingVitualEnv.py
@@ -0,0 +1,16 @@
+# Run below commands
+
+# pip install virtualenv
+# virtualenv --version
+# virtualenv myprojectenv
+
+# To ACTIVATE -> select env folder -> scripts -> activate.ps1
+# .\myprojectenv\Scripts\activate.ps1
+
+# To DEACTIVATE -> deactivate
+
+# pip freeze ---> to get all installed modules with version in current environment
+# pip freeze > requirement/txt   ---> will export data to txt file
+
+# Same file can be shared and install by others
+# pip install -r requirements.txt
